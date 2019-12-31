953c1246d86a692a5837d924818e39ac1d22f0c6
francescot <fratob85@gmail.com>
2019-12-31 18:38:01 +0100

    first commit

---
+++ src/server/models/index.js
@@ -0,0 +1,21 @@
+import Sequelize from 'sequelize';
+
+if (process.env.NODE_EAV === 'development') {
+    require('babel-plugin-require-context-hook/register')();
+}
+
+export default (sequelize) => {
+    let db = {};
+    const context = require.context('.', true, /^\.\/(?!index\.js).*\.js$/, 'sync');
+
+    context.keys().map(context).forEach(module => {
+        const model = module(sequelize, Sequelize);
+        db[model.name] = model;
+    });
+
+    Object.keys(db).forEach(modelName => {
+        if (db[modelName].associate) {
+            db[modelName].associate(db);
+        }
+    });
+}
